19e995ffc0331c494d88c4b68b06812aa0b3c776
mahumrana <mrana@newtech.coppellisd.com>
2022-03-07 14:38:07 -0600

    done

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,6 +1,7 @@
+
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
@@ -17,15 +18,24 @@ public class Lab01 {
 	
 	public static void problem01() {
 		
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter a Positive Integer:");
+		int Num1 = inKey.nextInt();
+		System.out.print("Enter another Positive Integer:");
+		int Num2 = inKey.nextInt();
+		double ans = Math.pow(Num1, Num2);
+		System.out.println(Num1+"^"+ Num2+ "="+ ans);
 	}
 	
 	
 	
 	
 	public static void problem02() {
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter a Positive Integer:");
+		double input = inKey.nextDouble();
+		double ans = Math.sqrt(input);
+		System.out.println("The Square Root of "+ input + " is "+ ans);
 		
 		
 	}
@@ -34,8 +44,16 @@ public class Lab01 {
 	
 
 	public static void problem03() {
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Length of side A: ");
+		double A = inKey.nextDouble();
+		System.out.print("Length of side B: ");
+		double B = inKey.nextDouble();
+		double A2 = Math.pow(A,2);
+		double B2 = Math.pow(B,2);
+		double C  = A2 + B2;
+		double C2 = Math.sqrt(C);
+		System.out.println("The third side is equal to" + C2);
 		
 
 	}
@@ -44,14 +62,25 @@ public class Lab01 {
 	
 	
 	public static void problem04() {
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		boolean zero = true;
+		int max = Integer.MIN_VALUE; 
+		int min = Integer.MAX_VALUE; 
+		int input = 1;
+		while(zero) {
+			System.out.println("Enter a  Number");
+			 input = inKey.nextInt();
+			if(input == 0) {
+				zero = false;
+			}
+			max = Math.max(input, max);
+			min = Math.min(input, min);
+		}
+		System.out.println("Max = "+ max);
+		System.out.println("Min = "+min);
 		
 	}
-	
-	
-	
-	
-	
-	
 }
+	
+	
+	
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,6 +1,7 @@
+
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab02 {
 
@@ -16,8 +17,27 @@ public class Lab02 {
 
 	
 	public static void problem01() {
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter a Positive Integer: ");
+		int num1 = inKey.nextInt();
+		System.out.println("Enter another Positive Integer: ");
+		int num2 = inKey.nextInt();
+		
+		int min = Math.min(num1, num2);
+		int max = Math.max(num1, num2);
+		
+		int scope = max -min+1;
 		
 		
+		// Random number from 13 to 28
+				//scope = max - min + 1
+				//scope = 28 - 13 + 1;
+				//min = 13;
+
+		for (int i = 0; i < 10; i++) {
+			int Random = (int) (Math.random() * scope + min);
+			System.out.println(Random + " ");
+		}	
 		
 		
 	}
@@ -26,8 +46,14 @@ public class Lab02 {
 	
 	
 	public static void problem02() {
-		
-		
+	Scanner inKey = new Scanner(System.in);
+	System.out.println("Enter the Radius of a cylinder: ");
+	int R = inKey.nextInt();
+	System.out.println("Enter the Height of a cylinder: ");
+	int H = inKey.nextInt();
+	
+	double V =  (H*Math.pow(R, 2)*Math.PI);
+	System.out.println(V);
 		
 	}
 
@@ -35,9 +61,23 @@ public class Lab02 {
 	
 
 	public static void problem03() {
-
+		Scanner inKey = new Scanner(System.in);
+		System.out.println(" Enter a number for X1: ");
+		double x1 = inKey.nextDouble();
+		// Y1
+		System.out.println(" Enter a number for Y1: ");
+		double y1 = inKey.nextDouble();
+		// X2
+		System.out.println(" Enter a number for X2: ");
+		double x2 = inKey.nextDouble();
+		//Y2
+		System.out.println(" Enter a number for Y2: ");
+		double y2 = inKey.nextDouble();
 		
 		
+		double ans = Math.sqrt(Math.pow(y2- y1, 2) + Math.pow(x2-x1,2));
+		
+		System.out.println(ans);
 	}
 
 
@@ -45,9 +85,23 @@ public class Lab02 {
 	
 	public static void problem04() {
 		
-		
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter a-value: ");
+		double a = inKey.nextDouble();
+		System.out.println();
+		System.out.print("Enter b-value: ");
+		double b = inKey.nextDouble();
+		System.out.println();
+		System.out.print("Enter c-value: ");
+		double c = inKey.nextDouble();
+		System.out.println();
+		double x1, x2;
+		double b2 = Math.pow(b, 2);
+		double sqrt = Math.sqrt(b2 - 4* a * c);
+		x1 = (-b + sqrt)/(2*a);
+		x2 = (-b - sqrt)/(2*a);
+		System.out.println(x1 =+ x1);
+		System.out.println(x2 =+ x2);
 	}
 	
 	
